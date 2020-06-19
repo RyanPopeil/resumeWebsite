b8524460c0596ddfa227fda7ab70b002775bcae6
RyanPopeil <rapopeil705@gmail.com>
2020-06-19 16:06:00 -0400

    Server works for index.html

--- server/server.js
+++ server/server.js
@@ -1,26 +1,13 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+const express = require('express');
+const app = express();
+const port = 1337;
+var bodyParser =require('body-parser');
 
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type' : 'text/plain','Access-Control-Allow-Origin' :'*'});
-	var q = url.parse(req.url, true);
-	var filename = "." + q.pathname;
-	var filetype = filename.slice(2);
-	filetype = filetype.slice(filetype.indexOf('.'));
-	fs.readFile(filename, function(err, data) {
-  	if (err) {
-      		res.writeHead(404, {'Content-Type': 'text/html'});
-      		return res.end("404 Not Found");
-    	}
-	if (filetype.includes('.jpeg')){
-		res.writeHead(200, {'Content-Type' : 'image/jpeg'});
-		return res.end(data);
-	}
-	if (filetype.includes('.html')){
-    		res.writeHead(200, {'Content-Type': 'text/html'});
-    		res.write(data);
-    		return res.end();
-	}
-  	});
-}).listen(8080);
+app.use(bodyParser.json({type: 'application/json'}));
+app.use(express.static('client/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile('index.html', {root: '../client/views' })
+})
+
+app.listen(port, () => console.log('Example app listening on port 1337!'))
